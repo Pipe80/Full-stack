fdf1419f3349112e208a785369645d07cf5388d7
Pirjo Mure <N5589@student.jamk.fi>
2023-02-01 09:58:04 +0200

    harjoitus 1_6

---
+++ Harjoitukset 1/tehtava_6.js
@@ -0,0 +1,10 @@
+let taulukko = [11, 22, 33, 44];
+let summa = 0;
+let lukumaara = 0;
+let keskiarvo;
+let tulosrivit = '';
+// Laske taulukon alkioiden lukumäärä ja summa for...of-rakenteella. 
+// Muuttujan tulosrivit sisältö tulee muodostaa for-rakenteella. 
+// Laske lisäksi keskiarvo keskiarvo-muuttujaan. 
+// Keskiarvo tulee tulostaa yhden desimaalin tarkkuudella (toFixed()-metodi)  
+// Liitä lopuksi muuttujiin keräämäsi data näytettäväksi P-elementissä getElementById/innerHTML-yhdistelmää käyttäen. Tuloksen tulisi olla oheisen kuvan mukainen. JavaScriptillä generoimasi sisältö tulisi olla näkyvissä HR-elementin (vaakaviiva) alapuolella.
